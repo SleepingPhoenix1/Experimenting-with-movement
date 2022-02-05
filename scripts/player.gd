e5e05d16024362ba3d2347497c4817a160f67a13
SleepingPhoenix1 <SleepingPhoenix2@gmail.com>
2022-02-05 20:49:52 +0200

    polishing movement
    added parameters to control movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
+signal landed
 
+var tick = false
 var current_speed = 0
 var is_moving = false
+var has_jumped = false
 var direction
 
 export var acceleration = 10
@@ -16,6 +19,7 @@ export var jump_slowing_down = 3
 export var jump_height : float
 export var jump_time_to_peak : float
 export var jump_time_to_descent : float
+export var lowfallMultiplier = 1
 
 onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
 onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
@@ -23,24 +27,35 @@ onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent
 
 
 func _ready():
-	pass
+	#for some reason has_jumped fires off without a reason
+	max_speed -= jump_slowing_down
 
 
 func _process(delta):
-	max_speed * 2
-
+	if has_jumped and is_on_floor():
+		max_speed += jump_slowing_down
+		has_jumped = false
+		tick = false
 
 func _physics_process(delta):
 	movement()
-	#print(max_speed)
+	#print(velocity.x)
 	
+	#lowers max speed if in air
 	if !is_on_floor():
-		velocity.y += get_gravity() * delta
-		
+		#gravity
+		velocity.y += get_gravity() * delta 
+		if has_jumped and !tick:
+			max_speed -= jump_slowing_down
+			tick = true
+		has_jumped = true
 	
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		jump()
 	
+	#low jumping
+	if has_jumped and Input.is_action_just_released("ui_jump") and velocity.y < 0:
+		velocity.y += lowfallMultiplier
 
 
 func movement():
@@ -82,16 +97,12 @@ func deceleration():
 	else: velocity.x = 0
 
 
-func get_gravity() -> float:
+func get_gravity() -> float:  #sets gravity type
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
-func jump():
+func jump(): #jumping
 	velocity.y = jump_velocity
-	#/max_speed -= jump_slowing_down
-
-
-
-
+	
 
 
 
